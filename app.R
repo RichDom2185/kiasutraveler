85fb5f725dcb3ef45adc11fe1918c64f14466fae
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-04 18:08:57 +0800

    Auto-format files

--- app.R
+++ app.R
@@ -3,26 +3,26 @@ library(shiny)
 source("ui/components.R")
 
 ui <- app(
-  div(class="box", "test"),
-  box(
-    input.text("pickUp", "Enter pick-up location...", label = "From:"),
-    input.text("dropOff", "Enter destination location...", label = "To:")
-  ),
-  textInput("input_id", NULL, "value"),
-  # textOutput("description"),
-  htmlOutput("description"),
+    div(class = "box", "test"),
+    box(
+        input.text("pickUp", "Enter pick-up location...", label = "From:"),
+        input.text("dropOff", "Enter destination location...", label = "To:")
+    ),
+    textInput("input_id", NULL, "value"),
+    # textOutput("description"),
+    htmlOutput("description"),
 )
 
 server <- function(input, output) {
- output$description <- renderText(paste(
-   "Searching for directions from",
-   strong(input$pickUp),
-   "to",
-   strong(input$dropOff)
- ))
+    output$description <- renderText(paste(
+        "Searching for directions from",
+        strong(input$pickUp),
+        "to",
+        strong(input$dropOff)
+    ))
 }
 
 shinyApp(ui, server, options = list(
-  port = 4000
-  # launch.browser = FALSE
+    port = 4000
+    # launch.browser = FALSE
 ))
--- ui/components.R
+++ ui/components.R
@@ -1,24 +1,28 @@
-app <- function(...) fillPage(
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "reset_bootstrap.css"),
-    tags$link(rel = "stylesheet", type = "text/css", href = "bulma.min.css")
-  ),
-  div(class = "root", ...))
+app <- function(...) {
+    fillPage(
+        tags$head(
+            tags$link(rel = "stylesheet", type = "text/css", href = "reset_bootstrap.css"),
+            tags$link(rel = "stylesheet", type = "text/css", href = "bulma.min.css")
+        ),
+        div(class = "root", ...)
+    )
+}
 
 box <- function(...) div(class = "box", ...)
 
-input.text <- function(id, placeholder = NULL, value = "", label = NULL) div(
-  class = "field",
-  if(is.null(label)) NULL else tags$label(class = "label", label),
-  div(
-    class = "control",
-    tags$input(
-      type = "text",
-      class = "input",
-      id = id,
-      placeholder = placeholder,
-      value
+input.text <- function(id, placeholder = NULL, value = "", label = NULL) {
+    div(
+        class = "field",
+        if (is.null(label)) NULL else tags$label(class = "label", label),
+        div(
+            class = "control",
+            tags$input(
+                type = "text",
+                class = "input",
+                id = id,
+                placeholder = placeholder,
+                value
+            )
+        )
     )
-  )
-)
-
+}
