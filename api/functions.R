69199a4d72f455fce51e1773e9132e8cc4e026f1
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-04 20:43:58 +0800

    Add getters for API data

--- api/functions.R
+++ api/functions.R
@@ -7,40 +7,139 @@ source("api/routes.R")
 ##########################################
 # Using API_ENDPOINT_FORWARD_GEOCODE_URL
 ##########################################
-getCoordinatesFromAddress <- function(address) {} # TODO:
+getCoordinatesFromAddress <- function(address) {
+    url <- API_ENDPOINT_FORWARD_GEOCODE_URL
+    params <- list(address = address)
+    response <- GET(url, query = params)
+    content(response)
+}
 
 
 ##########################################
 # Using API_ENDPOINT_REVERSE_GEOCODE_URL
 ##########################################
-getAddressFromCoordinates <- function(lat, lng) {} # TODO:
+getAddressFromCoordinates <- function(lat, lng) {
+    url <- API_ENDPOINT_REVERSE_GEOCODE_URL
+    params <- list(lat = lat, lng = lng)
+    response <- GET(url, query = params)
+    content(response)
+}
 
 
 ##########################################
 # Using API_ENDPOINT_RIDES_AVAILABILITY_UR
 ########################################## L
-getGrabServicesAvailability <- function(startLat, startLng, endLat, endLng) {} # TODO:
-getTaxiAvailability <- function(date, time) {} # TODO:
+getGrabServicesAvailability <- function(startLat, startLng, endLat, endLng) {
+    url <- API_ENDPOINT_RIDES_AVAILABILITY_URL
+    params <- list(
+        serviceType = "grab",
+        startLat = startLat,
+        startLng = startLng,
+        endLat = endLat,
+        endLng = endLng
+    )
+    response <- GET(url, query = params)
+    content(response)
+}
+
+getTaxiAvailability <- function() {
+    url <- API_ENDPOINT_RIDES_AVAILABILITY_URL
+    params <- list(
+        serviceType = "taxi"
+    )
+    response <- GET(url, query = params)
+    content(response)
+}
 
 
 ##########################################
 # Using API_ENDPOINT_ROUTES_URL
 ##########################################
-getCyclingDirections <- function(startLat, startLng, endLat, endLng) {} # TODO:
-getDrivingDirections <- function(startLat, startLng, endLat, endLng) {} # TODO:
-getWalkingDirections <- function(startLat, startLng, endLat, endLng) {} # TODO:
-getPublicTransportDirections <- function(startLat, startLng, endLat, endLng, date, time) {} # TODO:
+getCyclingDirections <- function(startLat, startLng, endLat, endLng) {
+    url <- API_ENDPOINT_ROUTES_URL
+    params <- list(
+        startLat = startLat,
+        startLng = startLng,
+        endLat = endLat,
+        endLng = endLng,
+        mode = "cycle"
+    )
+    response <- GET(url, query = params)
+    content(response)
+}
+
+getDrivingDirections <- function(startLat, startLng, endLat, endLng) {
+    url <- API_ENDPOINT_ROUTES_URL
+    params <- list(
+        startLat = startLat,
+        startLng = startLng,
+        endLat = endLat,
+        endLng = endLng,
+        mode = "drive"
+    )
+    response <- GET(url, query = params)
+    content(response)
+}
+
+getWalkingDirections <- function(startLat, startLng, endLat, endLng) {
+    url <- API_ENDPOINT_ROUTES_URL
+    params <- list(
+        startLat = startLat,
+        startLng = startLng,
+        endLat = endLat,
+        endLng = endLng,
+        mode = "walk"
+    )
+    response <- GET(url, query = params)
+    content(response)
+}
+
+getPublicTransportDirections <- function(startLat, startLng, endLat, endLng, date, time) {
+    url <- API_ENDPOINT_ROUTES_URL
+    params <- list(
+        startLat = startLat,
+        startLng = startLng,
+        endLat = endLat,
+        endLng = endLng,
+        mode = "pt",
+        date = date,
+        time = time,
+        vehicleType = "TRANSIT"
+    )
+    response <- GET(url, query = params)
+    content(response)
+}
 
 
 ##########################################
 # Using API_ENDPOINT_TRAFFIC_INCIDENTS_URL
 ##########################################
-getTrafficIncidents <- function() {} # TODO:
+getTrafficIncidents <- function() {
+    url <- API_ENDPOINT_TRAFFIC_INCIDENTS_URL
+    response <- GET(url)
+    content(response)
+}
 
 
 ##########################################
 # Using API_ENDPOINT_TRAFFIC_DENSITY_URL
 ##########################################
-# FIXME: `time parameter is not used yet`
-getBusPassengerDensity <- function() {} # TODO:
-getMrtPlatformDensity <- function(mrtLine, time) {} # TODO:
+getBusPassengerDensity <- function() {
+    url <- API_ENDPOINT_TRAFFIC_DENSITY_URL
+    params <- list(
+        type = "bus"
+    )
+    response <- GET(url, query = params)
+    content(response)
+}
+
+# FIXME: `time` parameter is not used yet
+getMrtPlatformDensity <- function(mrtLine, time) {
+    url <- API_ENDPOINT_TRAFFIC_DENSITY_URL
+    params <- list(
+        type = "platform",
+        mrt = mrtLine
+    )
+    response <- GET(url, query = params)
+    content(response)
+}
