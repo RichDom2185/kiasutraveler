5642226fe97ca422b81ffecd44c17e061e854b69
sechoi3445 <129940945+sechoi3445@users.noreply.github.com>
2023-04-11 17:09:29 +0800

    Update app.R

--- app.R
+++ app.R
@@ -11,7 +11,11 @@ source("logic/maps/maps.R")
 
 options(shiny.autoreload = TRUE)
 
-ui <- app(
+ui <- navbarPage("Kiasu Traveler",
+  tabPanel("Ride Hailing", plotOutput("ridehailing")),
+  tabPanel("Car Sharing", plotOutput("carsharing")),
+  tabPanel("Taxi",
+  app(
     useShinyjs(),
     stackElements(
         mapboxerOutput("map", height = "100vh"),
@@ -31,7 +35,7 @@ ui <- app(
                     tab("taxi", "Taxi"),
                     tab("pt", "Public Transport"),
                     tab("otherinsights", "Other Insights")
-                )
+                    )
             ),
             box(
                 style = "width: fit-content; position: absolute; bottom: 0; right: 0",
@@ -66,13 +70,56 @@ ui <- app(
                     ),
                     htmlOutput("description")
                 ),
+                
                 conditionalPanel(
-                    condition = "input.activeTab !== 'taxi'",
+                    condition = "input.activeTab == 'ridehailing'",
                     p("Coming soon!")
+                ),
+                conditionalPanel(
+                  condition = "input.activeTab == 'carsharing'",
+                  p("Coming soon!")
+                ),
+                conditionalPanel(
+                  condition = "input.activeTab == 'pt'",
+                  p("Coming soon!")
+                ),
+                
+                conditionalPanel(
+                  condition = "input.activeTab == 'otherinsights'",
+                  p("Coming soon!")
                 )
+                
             )
         )
     )
+)), 
+    tabPanel("Public transport", plotOutput("pt")),
+    navbarMenu("Other insights", 
+               tabPanel("Weather",
+                        tabsetPanel(
+                          tabPanel("2 hour forecast", plotOutput("2hourforecast"))
+                        )),
+               tabPanel("Crowd",
+                        tabsetPanel(
+                          tabPanel("Bus Volume", plotOutput("busvolume")),
+                          tabPanel("MRT Volume", plotOutput("MRT Volume"))
+                        )),
+               tabPanel("Traffic Incidents",
+                        tabsetPanel(
+                          tabPanel("Wordcloud", 
+                                   sidebarLayout(
+                                     sidebarPanel("explanation"),
+                                     mainPanel(plotOutput("wordcloud")))),
+                          tabPanel("Incident Causes Today", 
+                                   sidebarLayout(
+                                     sidebarPanel(
+                                       "explanation"
+                                     ),
+                                     mainPanel(plotOutput("incidentbar"))
+                                   )),
+                          tabPanel("Incident Map Today", mapboxerOutput("incident_map", height = "100vh")))
+               ))
+
 )
 
 server <- function(input, output) {
@@ -118,34 +165,126 @@ server <- function(input, output) {
         strong(input$layer),
         "..."
     ))
+    
 
     output$map <- renderMapboxer({
-        mapbox <- getSingaporeMap() %>% add_navigation_control(pos = "bottom-left")
-        if (input$layer == "Heatmap") {
+      mapbox <- getSingaporeMap() %>% add_navigation_control(pos = "bottom-left")
+          if (input$layer == "Heatmap") {
             mapbox <- mapbox %>%
-                add_layer(
-                    list(
-                        id = "heatmap_layer",
-                        type = "heatmap",
-                        source = as_mapbox_source(df()),
-                        paint = list(
-                            "heatmap-opacity" = 0.3,
-                            "heatmap-radius" = 10
-                        )
-                    )
+              add_layer(
+                list(
+                  id = "heatmap_layer",
+                  type = "heatmap",
+                  source = as_mapbox_source(df()),
+                  paint = list(
+                    "heatmap-opacity" = 0.3,
+                    "heatmap-radius" = 10
+                  )
                 )
-        } else if (input$layer == "Point") {
+              )
+          } else if (input$layer == "Point") {
             mapbox <- mapbox %>%
-                add_circle_layer(
-                    id = "taxis_points",
-                    source = as_mapbox_source(df()),
-                    circle_color = "red",
-                    circle_radius = 4,
-                    circle_opacity = 0.2
-                ) %>%
-                add_tooltips("taxis_points", "{{type}}")
-        }
+              add_circle_layer(
+                id = "taxis_points",
+                source = as_mapbox_source(df()),
+                circle_color = "red",
+                circle_radius = 4,
+                circle_opacity = 0.2
+              ) %>%
+              add_tooltips("taxis_points", "{{type}}")
+          }
+        
     })
+    
+    
+    output$wordcloud <- renderPlot({
+      traffic_data <- read.csv("causes-of-accidents-by-severity-of-injury-sustained.csv")
+      names(traffic_data)[names(traffic_data) == "causes_of_accident"] <- "C"
+      
+      #Manual Cleaning of Data for WordCloud Words appear simplified
+      traffic_data$C <- gsub("Carrying Out Work on the Road without Proper Attire or Sufficient Warning Signs", "Roadwork", traffic_data$C)
+      traffic_data$C <- gsub("Causes Attributed to Road Conditions", "Misc", traffic_data$C)
+      traffic_data$C <- gsub("Causes Attributed to Vehicles", "Vehicle Breakdown", traffic_data$C)
+      traffic_data$C <- gsub("Changing Lane without Due Care", "Diversion", traffic_data$C)
+      traffic_data$C <- gsub("Crossing Heedless of Traffic", "Pedestrian", traffic_data$C)
+      traffic_data$C <- gsub("Crossing In Front or Behind a Vehicle which Obstructs View", "Pedestrian", traffic_data$C)
+      traffic_data$C <- gsub("Crossing Within Pedestrian Crossing When Red Man Lighted", "Pedestrian", traffic_data$C)
+      traffic_data$C <- gsub("Disobeying Traffic Light Signals Resulting in Accidents with Vehicle", "Accident", traffic_data$C)
+      traffic_data$C <- gsub("Driving under the Influence of Alcohol", "Alcohol", traffic_data$C)
+      traffic_data$C <- gsub("Failing to Give Way to Traffic with Right of Way", "Accident", traffic_data$C)
+      traffic_data$C <- gsub("Failing to Keep a Proper Lookout", "Diversion", traffic_data$C)
+      traffic_data$C <- gsub("Failing to Use Available Pedestrian Crossing", "Pedestrian", traffic_data$C)
+      traffic_data$C <- gsub("Failing to Have Proper Control", "Accident", traffic_data$C)
+      traffic_data$C <- gsub("Following Too Close to Vehicle In Front", "Accident", traffic_data$C)
+      traffic_data$C <- gsub("Overtaking without Due Care", "Diversion", traffic_data$C)
+      traffic_data$C <- gsub("Other causes attributed to drivers, riders or pedal cyclists", "Accident", traffic_data$C)
+      traffic_data$C <- gsub("Other Causes of Accidents Attributed to Pedestrians", "Pedestrians", traffic_data$C)
+      traffic_data$C <- gsub("Turning Vehicle & Failing to Give Way to Pedestrian During Green Man", "Accident", traffic_data$C)
+      traffic_data$C <- gsub("Playing on The Road or Carpark", "Pedestrian", traffic_data$C)
+      traffic_data$C <- gsub("Turning Without Due Care", "Diversion", traffic_data$C)
+      traffic_data$C <- gsub("Under the Influence of Alcohol", "Alcohol", traffic_data$C)
+      traffic_data$C <- gsub("Under the Influence of Drugs/Intoxicated Substance", "Substance", traffic_data$C)
+      traffic_data$C <- gsub("Using PMD to Travel on Road", "PMD", traffic_data$C)
+      traffic_data$C <- gsub("Other Causes", "Misc", traffic_data$C)
+      
+      traffic_wordcloud <- traffic_data %>% group_by(C) %>% summarise(count = sum(number_of_accidents))
+      names(traffic_wordcloud)[names(traffic_wordcloud) == "C"] <- "Causes"
+      #traffic_wordcloud$Causes
+      
+      set.seed(1234)
+      wordcloud(words = traffic_wordcloud$Causes, freq = traffic_wordcloud$count, min.freq = 1, max.words=200, random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "RdBu"))
+    })
+    
+    output$incidentbar <- renderPlot({
+      #Import API
+      api_key <- "REDACTED"
+      
+      url_incidents <- "http://datamall2.mytransport.sg/ltaodataservice/TrafficIncidents"
+      response <- GET(url_incidents, add_headers(AccountKey=api_key))
+      data_incidents <- fromJSON(content(response, as = "text"))
+      data_incidents <- as.data.frame(data_incidents)
+      #Extract location
+      df_incidents <- data.frame(lat = data_incidents$value.Latitude, long = data_incidents$value.Longitude)
+      traffic_counted2 <- data_incidents %>% group_by(value.Type) %>% summarise(Count = n())
+      
+      #Account for Incident Types not occured on the day
+      all_incidents <- c('Accident', 'Roadwork', 'Vehicle Breakdown', 'Weather', 'Obstacle', 'Road Block', 'Heavy Traffic', 'Misc', 'Diversion', 'Unattended Vehicle')
+      not_today <- gsub(paste(traffic_counted2$value.Type, collapse = "|"), "", all_incidents)
+      not_today <- not_today[nzchar(not_today)]
+      not_today <- data.frame(value.Type = not_today, Count = 0)
+      traffic_counted2 <- rbind(traffic_counted2, not_today)
+      
+      traffic_count_live <- ggplot(traffic_counted2) + geom_bar(aes(x = value.Type, y = Count), stat = 'identity', fill = "#0171bb") + geom_text(aes(label = Count, x = value.Type, y = Count), vjust = -0.2, colour = "black")
+      
+      traffic_count_live + theme_classic() + labs(title = "Traffic Incidents Today", x = "Type of Incident", y = "Number of Accidents") + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    })
+    
+    output$incident_map <- renderMapboxer({
+      #Import API
+      api_key <- "REDACTED"
+      
+      url_incidents <- "http://datamall2.mytransport.sg/ltaodataservice/TrafficIncidents"
+      response <- GET(url_incidents, add_headers(AccountKey=api_key))
+      data_incidents <- fromJSON(content(response, as = "text"))
+      data_incidents <- as.data.frame(data_incidents)
+      df_incidents <- data.frame(lat = data_incidents$value.Latitude, long = data_incidents$value.Longitude)
+      
+      # Convert data to an sf object
+      df_incident_sf <- st_as_sf(df_incidents, coords = c("long", "lat"), crs = 4326)
+      df_incident_sf$message <- data_incidents$value.Message
+      
+      # Add a heatmap layer to the Mapbox map
+      traffic_map <- as_mapbox_source(df_incident_sf) %>%
+        mapboxer(style = getBasemap("voyager"), center = COORDINATES_SINGAPORE, zoom = 10) %>%
+        add_navigation_control() %>%
+        add_circle_layer(
+          id = "Incidents",
+          circle_color = "#0171bb",
+          popup = "<p>{{message}}</p>"
+        )
+    })
+    
+    
 }
 
 shinyApp(ui, server, options = list(
