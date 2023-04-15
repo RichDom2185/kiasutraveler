6587ce066f2f84e51edbdb2d85e341ab0145485f
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-15 15:39:49 +0800

    Refactor taxi tab logic to separate file

--- app.R
+++ app.R
@@ -1,5 +1,3 @@
-library(hms)
-library(jsonlite)
 library(shiny)
 library(shinyjs)
 library(shinyTime)
@@ -7,9 +5,7 @@ library(mapboxer)
 
 source("api/functions.R")
 source("ui/components/bulma.R")
-source("logic/maps/maps.R")
 
-# Must come after the libraries are imported
 source("ui/pages/taxi.R")
 
 options(shiny.autoreload = TRUE)
@@ -57,72 +53,7 @@ server <- function(input, output) {
         addCssClass(id = currentActiveTab(), class = "is-active")
     })
 
-    data <- reactive({
-        # TODO: Remove this in production
-        print("Firing API call...")
-        fromJSON(formatUrl(input, "date", "time"))
-    })
-
-    df <- reactive({
-        # FIXME: Beware of side effect due to
-        #        setting of global variable
-        setApiMode("auto")
-
-        df <- data()$features$geometry$coordinates %>% data.frame()
-
-        colnames(df) <- c("lng", "lat")
-        df$type <- "Taxi"
-
-        return(df)
-    })
-
-    getSingaporeMap <- reactive(
-        mapboxer(
-            style = getBasemap(input$mapType),
-            center = COORDINATES_SINGAPORE,
-            zoom = 10,
-            pitch = 15,
-            minZoom = 7
-        )
-    )
-
-    output$description <- renderText(paste(
-        "Getting taxi availability on",
-        strong(input$date),
-        "at",
-        strong(getTime(input, "time")),
-        "as a",
-        strong(input$layer),
-        "..."
-    ))
-
-    output$map <- renderMapboxer({
-        mapbox <- getSingaporeMap() %>% add_navigation_control(pos = "bottom-left")
-        if (input$layer == "Heatmap") {
-            mapbox <- mapbox %>%
-                add_layer(
-                    list(
-                        id = "heatmap_layer",
-                        type = "heatmap",
-                        source = as_mapbox_source(df()),
-                        paint = list(
-                            "heatmap-opacity" = 0.3,
-                            "heatmap-radius" = 10
-                        )
-                    )
-                )
-        } else if (input$layer == "Point") {
-            mapbox <- mapbox %>%
-                add_circle_layer(
-                    id = "taxis_points",
-                    source = as_mapbox_source(df()),
-                    circle_color = "red",
-                    circle_radius = 4,
-                    circle_opacity = 0.2
-                ) %>%
-                add_tooltips("taxis_points", "{{type}}")
-        }
-    })
+    updateTaxiTab(input, output)
 }
 
 shinyApp(ui, server, options = list(
--- ui/pages/taxi.R
+++ ui/pages/taxi.R
@@ -1,3 +1,10 @@
+library(hms)
+library(jsonlite)
+library(mapboxer)
+library(shiny)
+
+source("logic/maps/maps.R")
+
 taxiTabContent <- conditionalPanel(
     condition = "input.activeTab == 'taxi'",
     columns(
@@ -29,3 +36,72 @@ taxiTabContent <- conditionalPanel(
     ),
     htmlOutput("description")
 )
+
+updateTaxiTab <- function(input, output) {
+    data <- reactive({
+        # TODO: Remove this in production
+        print("Firing API call...")
+        fromJSON(formatUrl(input, "date", "time"))
+    })
+
+    df <- reactive({
+        # FIXME: Beware of side effect due to
+        #        setting of global variable
+        setApiMode("auto")
+
+        df <- data()$features$geometry$coordinates %>% data.frame()
+
+        colnames(df) <- c("lng", "lat")
+        df$type <- "Taxi"
+
+        return(df)
+    })
+
+    output$description <- renderText(paste(
+        "Getting taxi availability on",
+        strong(input$date),
+        "at",
+        strong(getTime(input, "time")),
+        "as a",
+        strong(input$layer),
+        "..."
+    ))
+
+    getSingaporeMap <- reactive(
+        mapboxer(
+            style = getBasemap(input$mapType),
+            center = COORDINATES_SINGAPORE,
+            zoom = 10,
+            pitch = 15,
+            minZoom = 7
+        )
+    )
+
+    output$map <- renderMapboxer({
+        mapbox <- getSingaporeMap() %>% add_navigation_control(pos = "bottom-left")
+        if (input$layer == "Heatmap") {
+            mapbox <- mapbox %>%
+                add_layer(
+                    list(
+                        id = "heatmap_layer",
+                        type = "heatmap",
+                        source = as_mapbox_source(df()),
+                        paint = list(
+                            "heatmap-opacity" = 0.3,
+                            "heatmap-radius" = 10
+                        )
+                    )
+                )
+        } else if (input$layer == "Point") {
+            mapbox <- mapbox %>%
+                add_circle_layer(
+                    id = "taxis_points",
+                    source = as_mapbox_source(df()),
+                    circle_color = "red",
+                    circle_radius = 4,
+                    circle_opacity = 0.2
+                ) %>%
+                add_tooltips("taxis_points", "{{type}}")
+        }
+    })
+}
