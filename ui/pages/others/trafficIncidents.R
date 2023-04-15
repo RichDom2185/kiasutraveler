baec4fea3d9378f7957099f258afbf6856e9e4d2
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-15 22:32:30 +0800

    Clean up word cloud code

--- ui/pages/others/trafficIncidents.R
+++ ui/pages/others/trafficIncidents.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(shiny)
+library(stringr)
 library(wordcloud)
 
 trafficIncidentsTabContent <- conditionalPanel(
@@ -86,44 +87,48 @@ updateTrafficIncidentsTab <- function(input, output) {
             # Wordcloud
             "Word Cloud" = {
                 traffic_data <- read.csv("data/causes-of-accidents-by-severity-of-injury-sustained.csv")
-                names(traffic_data)[names(traffic_data) == "causes_of_accident"] <- "C"
 
                 # Manual Cleaning of Data for WordCloud Words appear simplified
-                traffic_data$C <- gsub("Carrying Out Work on the Road without Proper Attire or Sufficient Warning Signs", "Roadwork", traffic_data$C)
-                traffic_data$C <- gsub("Causes Attributed to Road Conditions", "Misc", traffic_data$C)
-                traffic_data$C <- gsub("Causes Attributed to Vehicles", "Vehicle Breakdown", traffic_data$C)
-                traffic_data$C <- gsub("Changing Lane without Due Care", "Diversion", traffic_data$C)
-                traffic_data$C <- gsub("Crossing Heedless of Traffic", "Pedestrian", traffic_data$C)
-                traffic_data$C <- gsub("Crossing In Front or Behind a Vehicle which Obstructs View", "Pedestrian", traffic_data$C)
-                traffic_data$C <- gsub("Crossing Within Pedestrian Crossing When Red Man Lighted", "Pedestrian", traffic_data$C)
-                traffic_data$C <- gsub("Disobeying Traffic Light Signals Resulting in Accidents with Vehicle", "Accident", traffic_data$C)
-                traffic_data$C <- gsub("Driving under the Influence of Alcohol", "Alcohol", traffic_data$C)
-                traffic_data$C <- gsub("Failing to Give Way to Traffic with Right of Way", "Accident", traffic_data$C)
-                traffic_data$C <- gsub("Failing to Keep a Proper Lookout", "Diversion", traffic_data$C)
-                traffic_data$C <- gsub("Failing to Use Available Pedestrian Crossing", "Pedestrian", traffic_data$C)
-                traffic_data$C <- gsub("Failing to Have Proper Control", "Accident", traffic_data$C)
-                traffic_data$C <- gsub("Following Too Close to Vehicle In Front", "Accident", traffic_data$C)
-                traffic_data$C <- gsub("Overtaking without Due Care", "Diversion", traffic_data$C)
-                traffic_data$C <- gsub("Other causes attributed to drivers, riders or pedal cyclists", "Accident", traffic_data$C)
-                traffic_data$C <- gsub("Other Causes of Accidents Attributed to Pedestrians", "Pedestrians", traffic_data$C)
-                traffic_data$C <- gsub("Turning Vehicle & Failing to Give Way to Pedestrian During Green Man", "Accident", traffic_data$C)
-                traffic_data$C <- gsub("Playing on The Road or Carpark", "Pedestrian", traffic_data$C)
-                traffic_data$C <- gsub("Turning Without Due Care", "Diversion", traffic_data$C)
-                traffic_data$C <- gsub("Under the Influence of Alcohol", "Alcohol", traffic_data$C)
-                traffic_data$C <- gsub("Under the Influence of Drugs/Intoxicated Substance", "Substance", traffic_data$C)
-                traffic_data$C <- gsub("Using PMD to Travel on Road", "PMD", traffic_data$C)
-                traffic_data$C <- gsub("Other Causes", "Misc", traffic_data$C)
+                traffic_data$causes_of_accident <- traffic_data$causes_of_accident %>% str_replace_all(c(
+                    "Carrying Out Work on the Road without Proper Attire or Sufficient Warning Signs" = "Roadwork",
+                    "Causes Attributed to Road Conditions" = "Misc",
+                    "Causes Attributed to Vehicles" = "Vehicle Breakdown",
+                    "Changing Lane without Due Care" = "Diversion",
+                    "Crossing Heedless of Traffic" = "Pedestrian",
+                    "Crossing In Front or Behind a Vehicle which Obstructs View" = "Pedestrian",
+                    "Crossing Within Pedestrian Crossing When Red Man Lighted" = "Pedestrian",
+                    "Disobeying Traffic Light Signals Resulting in Accidents with Vehicle" = "Accident",
+                    "Driving under the Influence of Alcohol" = "Alcohol",
+                    "Failing to Give Way to Traffic with Right of Way" = "Accident",
+                    "Failing to Keep a Proper Lookout" = "Diversion",
+                    "Failing to Use Available Pedestrian Crossing" = "Pedestrian",
+                    "Failing to Have Proper Control" = "Accident",
+                    "Following Too Close to Vehicle In Front" = "Accident",
+                    "Overtaking without Due Care" = "Diversion",
+                    "Other causes attributed to drivers, riders or pedal cyclists" = "Accident",
+                    "Other Causes of Accidents Attributed to Pedestrians" = "Pedestrians",
+                    "Turning Vehicle & Failing to Give Way to Pedestrian During Green Man" = "Accident",
+                    "Playing on The Road or Carpark" = "Pedestrian",
+                    "Turning Without Due Care" = "Diversion",
+                    "Under the Influence of Alcohol" = "Alcohol",
+                    "Under the Influence of Drugs/Intoxicated Substance" = "Substance",
+                    "Using PMD to Travel on Road" = "PMD",
+                    "Other Causes" = "Misc"
+                ))
+
 
                 traffic_wordcloud <- traffic_data %>%
-                    group_by(C) %>%
+                    group_by(causes_of_accident) %>%
                     summarise(count = sum(number_of_accidents))
-                names(traffic_wordcloud)[names(traffic_wordcloud) == "C"] <- "Causes"
-                # traffic_wordcloud$Causes
 
                 set.seed(1000)
                 wordcloud(
-                    words = traffic_wordcloud$Causes, freq = traffic_wordcloud$count, min.freq = 1, max.words = 200,
-                    random.order = FALSE, rot.per = 0.35, colors = brewer.pal(8, "RdBu")
+                    words = traffic_wordcloud$causes_of_accident,
+                    freq = traffic_wordcloud$count,
+                    min.freq = 1,
+                    max.words = 200,
+                    random.order = FALSE,
+                    rot.per = 0.35, colors = brewer.pal(8, "RdBu")
                 )
             },
             "Bar Chart" = {
