982e76732acb4dfa1ce4f81f9214d2239e24acbc
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-15 18:19:23 +0800

    Show Grab services on ride-hailing page

--- ui/pages/rideHailing.R
+++ ui/pages/rideHailing.R
@@ -1,10 +1,35 @@
 library(shiny)
 
+## FUNCTIONS
+
+getServicesBetweenPostalCodes <- function(fromPostalCode, toPostalCode) {
+    fromLocation <- getCoordinatesFromAddress(fromPostalCode)[[1]]
+    toLocation <- getCoordinatesFromAddress(toPostalCode)[[1]]
+
+    result <- do.call(getGrabServicesAvailability, list(
+        fromLocation$lat,
+        fromLocation$lng,
+        toLocation$lat,
+        toLocation$lng
+    ))
+    result$services
+}
+
+## UI AND LOGIC
+
 rideHailingTabContent <- conditionalPanel(
     condition = "input.activeTab === 'rideHailing'",
-    p("RideHailing content coming soon!")
+    htmlOutput("rideHailingDescription"),
+    tableOutput("rideHailingTable")
 )
 
 updateRideHailingTab <- function(input, output) {
-    # TODO: Fill up
+    output$rideHailingTable <- renderTable({
+        withProgress(message = "Checking available Grab services...", {
+            pickUp <- isolate(input$pickUp)
+            dropOff <- isolate(input$dropOff)
+
+            getServicesBetweenPostalCodes(pickUp, dropOff)
+        })
+    })
 }
