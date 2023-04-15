a1361757f98da5f5d74ef71140793ef6dce299a2
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-16 00:22:07 +0800

    Clean up monthly rainy days plot

--- ui/pages/others/weather.R
+++ ui/pages/others/weather.R
@@ -1,38 +1,33 @@
+library(dplyr)
 library(ggplot2)
 library(shiny)
 
 weatherTabContent <- conditionalPanel(
     condition = "input.othersTab === 'weather'",
-    p("Weather tab data coming soon"),
     plotOutput("weatherPlot")
 )
 
 updateWeatherTab <- function(input, output) {
     output$weatherPlot <- renderPlot({
-        # read the CSV file
-        rainfall <- read.csv("data/MonthlyRainfall.csv")
-
-        # convert month to date format
-        rainfall$month <- as.Date(paste(rainfall$month, "-01", sep = ""), format = "%Y-%m-%d")
-
-        # filter the data for time period from 2013 to 2022
-        rainfall <- rainfall[format(rainfall$month, "%Y") >= "2013" & format(rainfall$month, "%Y") <= "2022", ]
-
-        # calculate average number of rainy days for each month
-        monthly_avg <- aggregate(no_of_rainy_days ~ format(month, "%b"), data = rainfall, FUN = mean)
-
-        # rename the column names
-        colnames(monthly_avg) <- c("Month", "Average_Rainy_Days")
+        rainfall_data <- read.csv("data/MonthlyRainfall.csv")
+        monthly_avg <- rainfall_data %>%
+            tidyr::separate(month, into = c("year", "month"), sep = "-") %>%
+            mutate(year = as.numeric(year), month = as.factor(month)) %>%
+            # filter the data for time period from 2013 to 2022
+            filter(year >= 2013 & year <= 2022) %>%
+            # calculate average number of rainy days for each month
+            group_by(month) %>%
+            summarise(ave_no_of_rainy_days = mean(no_of_rainy_days))
+        levels(monthly_avg$month) <- base::month.abb
 
         # plot the monthly averages
-        ggplot(monthly_avg, aes(x = Month, y = Average_Rainy_Days, fill = Average_Rainy_Days)) +
+        ggplot(monthly_avg, aes(x = month, y = ave_no_of_rainy_days, fill = ave_no_of_rainy_days)) +
             geom_bar(stat = "identity") +
             scale_fill_gradient(low = "#0171BB", high = "#E76963") +
             ggtitle("Average Monthly Rainy Days from 2013 to 2022") +
             ylab("Average Number of Rainy Days") +
             xlab("Month") +
             theme_classic() +
-            scale_y_continuous(expand = c(0, 0)) +
-            scale_x_discrete(limits = month.abb)
+            scale_y_continuous(expand = c(0, 0))
     })
 }
