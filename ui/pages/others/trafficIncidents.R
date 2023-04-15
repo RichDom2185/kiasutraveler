6294020a13a8ed963db93e86928e9419486af80b
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-15 21:51:48 +0800

    Populate more content to traffic incidents page
    Co-authored-by: sechoi3445 <129940945+sechoi3445@users.noreply.github.com>

--- ui/pages/others/trafficIncidents.R
+++ ui/pages/others/trafficIncidents.R
@@ -2,6 +2,15 @@ library(shiny)
 
 trafficIncidentsTabContent <- conditionalPanel(
     condition = "input.othersTab === 'trafficIncidents'",
+    # Wordcloud
+    h2("Traffic Incident Causes in Singapore", style = "font-weight:bold; font-size:20px;"), br(),
+    p("The following wordcloud illustrates some of the most common traffic incident causes in Singapore from 2012 to 2018. The most common cause appears to be Diversion and Accidents"),
+    br(),
+    p("Incidents categorised under 'Diversion' include 'Turning / Changing Lanes without Due Care'."), br(),
+    p("Incidents categorised under 'Accidents' include 'Disobeying Traffic Signals' or 'Failing to give way to a pedestrian'."), br(),
+    p("Take account of these statistics when you are on the road!"),
+    plotOutput("wordcloud"),
+    # Live Incident Causes
     h1("Live Traffic Incident Causes", style = "font-weight:bold; font-size:20px;"), br(),
     p("The bar graph illustrates live traffic incident causes right now. The data is updated every two minutes."),
     br(),
@@ -10,7 +19,9 @@ trafficIncidentsTabContent <- conditionalPanel(
     strong(paste(
         "Getting traffic incident information on", format(Sys.Date(), "%A, %B %d, %Y"),
         "at", format(Sys.time(), "%I:%M %p"), "..."
-    ))
+    )),
+    plotOutput("incidentbar")
+    # TODO: Live Traffic Incidents
 )
 
 updateTrafficIncidentsTab <- function(input, output) {
