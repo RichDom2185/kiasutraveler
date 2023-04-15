d56c291a9dfbb48af5c268644acd1104fe686abe
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-15 21:47:00 +0800

    Refactor traffic incidents tab
    Move the code for the tab to a separate file.

--- ui/pages/others.R
+++ ui/pages/others.R
@@ -1,25 +1,7 @@
 library(shiny)
 library(shinyjs)
 
-## FUNCTIONS
-
-trafficIncidentsTabContent <- conditionalPanel(
-    condition = "input.othersTab === 'trafficIncidents'",
-    h1("Live Traffic Incident Causes", style = "font-weight:bold; font-size:20px;"), br(),
-    p("The bar graph illustrates live traffic incident causes right now. The data is updated every two minutes."),
-    br(),
-    p("Observe the various incidents happening now to get a better idea of the road congestion situation."),
-    p("Click on the 'Live Traffic Incidents' tab to find out where these incidents occured!"), br(),
-    strong(paste(
-        "Getting traffic incident information on", format(Sys.Date(), "%A, %B %d, %Y"),
-        "at", format(Sys.time(), "%I:%M %p"), "..."
-    ))
-)
-
-updateTrafficIncidentsTab <- function(input, output) {
-}
-
-## UI AND LOGIC
+source("ui/pages/others/trafficIncidents.R")
 
 othersTabContent <- list(
     setDefaultTab("othersTab", "trafficIncidents"),
