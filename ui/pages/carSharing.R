7b365da9cd898218056b4f35486ba01ea156dd80
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-16 10:33:38 +0800

    Add GetGo availability layer

--- ui/pages/carSharing.R
+++ ui/pages/carSharing.R
@@ -6,5 +6,27 @@ carSharingTabContent <- conditionalPanel(
 )
 
 updateCarSharingTab <- function(input, output) {
-    # TODO: Fill up
+    # TODO: Refactor
+    output$map <- renderMapboxer({
+        map <- mapboxer(
+            # TODO: Remove hardcoding
+            style = getBasemap("voyager"),
+            center = COORDINATES_SINGAPORE,
+            zoom = 10,
+            pitch = 15,
+            minZoom = 7
+        ) %>%
+            add_navigation_control(pos = "bottom-left") %>%
+            add_circle_layer(
+                source = getAvailableGetgoVehicles()$data %>% as_mapbox_source(),
+                popup = paste0(
+                    strong("{{vehicleName}}"),
+                    "<br>",
+                    tags$em("{{vehicleNo}}"),
+                    "<br>Capacity: {{capacity}}"
+                ),
+                circle_color = "red",
+                circle_radius = 5
+            )
+    })
 }
