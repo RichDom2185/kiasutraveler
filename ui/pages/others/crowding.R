bd35b3f1a77651d82e91e71a4633eb6ab87a8ea5
sechoi3445 <129940945+sechoi3445@users.noreply.github.com>
2023-04-16 21:42:03 +0800

    Update crowding.R

--- ui/pages/others/crowding.R
+++ ui/pages/others/crowding.R
@@ -4,10 +4,19 @@ library(shiny)
 
 crowdingTabContent <- conditionalPanel(
     condition = "input.othersTab === 'crowding'",
-    p("Bus Crowding Plot"),
+    strong("Bus Crowding Plot"),
     plotOutput("busCrowdingPlot", width = "600px", height = "400px"),
-    p("MRT Crowding Plot"),
-    plotOutput("mrtCrowdingPlot", width = "600px", height = "400px")
+    p("Buses are the most crowded from 7am to 8am and 5pm to 6pm on weekdays. 
+    Buses are consistently crowded throughout the day on weekends especially 
+    5pm to 7pm. However, you will definitely see less people at the platforms. 
+    If possible, plan your travel time accordingly if you wish to avoid the crowd!"),
+    br(),
+    strong("MRT Crowding Plot"),
+    plotOutput("mrtCrowdingPlot", width = "600px", height = "400px"),
+    p("MRT is the most crowded from 7am to 8am and 5pm to 6pm on weekdays. 
+    MRT is consistently crowded throughout the day on weekends but you will 
+    definitely see less people at the platforms. If possible, plan your travel time accordingly 
+    if you wish to avoid the crowd!")
 )
 
 updateCrowdingTab <- function(input, output) {
